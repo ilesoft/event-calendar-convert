93379d9ba94d5a25d8c57280c02a3f5112d1890e
Ile <ilmari.marttila@gmail.com>
2022-12-14 12:23:17 +0200

    Add converter

---
+++ convert.hs
@@ -0,0 +1,28 @@
+import System.IO
+
+handleSplittedLine :: [String] -> String
+handleSplittedLine [] = ""
+handleSplittedLine [header] = "\n    <th>" ++ header ++ "</th>"
+handleSplittedLine parts
+    | length parts > 2 = "\n    <td>" ++ head parts ++ "</td>\n    <td>" ++ foldl1 (\a b -> a ++ "," ++ b) (tail (init parts)) ++ "</td>\n    <td>" ++ last parts ++ "</td>"
+
+split :: String -> [String]
+split s =  case dropWhile (== ',') s of
+                      "" -> []
+                      s' -> w : split s''
+                            where (w, s'') = break (== ',') s'
+
+handleLine :: String -> String
+handleLine line = "  <tr>" ++ filter (/= '"') (handleSplittedLine(split line)) ++ "\n  </tr>"
+
+handleFile :: String -> String
+handleFile input = unlines $ map handleLine $ filter (/= ",,") $ lines input
+
+main = do
+    -- those excel files have carriage returns
+    hSetNewlineMode stdin universalNewlineMode
+
+    contents <- getContents
+    putStrLn "<table>"
+    putStr $ handleFile contents
+    putStrLn "</table>"
